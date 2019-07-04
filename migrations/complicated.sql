5c4a13b37b2065349f717ca2a481732893677447
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-07-04 13:02:16 +0300

    fixed requests and implemet generate script

--- migrations/complicated.sql
+++ migrations/complicated.sql
@@ -4,15 +4,16 @@ SELECT t.flight, t.dep_date, SUM(d.miles) FROM ticket AS t
 -- 2
 SELECT dep_airport, MONTH(dep_date), class, COUNT(*) FROM ticket
     WHERE YEAR(dep_date) = 2014 GROUP BY dep_airport, MONTH(dep_date), class;
--- 3
+-- 3 XXX = 10
 SELECT p.* FROM profile AS p WHERE p.uid IN (
     SELECT profile_id FROM detail d JOIN ticket t ON (d.ticket_id = t.uid) WHERE t.flight = 10 AND d.price = (
     SELECT MAX(d.price) FROM detail AS d JOIN ticket AS t ON (d.ticket_id = t.uid) WHERE t.flight = 10));
 -- 4
 SELECT p.* FROM profile AS p LEFT JOIN detail AS d ON (p.uid = d.profile_id) WHERE d.profile_id IS NULL;
 
--- 5 ????? todo: check it
-SELECT p.* FROM profile AS p LEFT JOIN detail AS d ON (p.uid = d.profile_id) WHERE YEAR(buy_date) = 2014 AND MONTH(buy_date) = 3;
+-- 5
+SELECT p.* FROM profile AS p WHERE uid NOT IN (
+    SELECT profile_id FROM detail AS d WHERE YEAR(d.buy_date) = 2014 AND MONTH(d.buy_date) = 3);
 -- 6
 CREATE OR REPLACE VIEW often_pas AS
     SELECT p.uid, COUNT(*) as num FROM profile AS p JOIN detail AS d ON (p.uid = d.profile_id)
